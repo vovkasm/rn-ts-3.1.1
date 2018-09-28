780740360c57b1827dadfc9cd39821bfcf50950a
Vladimir Timofeev <vovkasm@gmail.com>
2018-09-28 11:20:26 +0300

    add error to comments

--- index.tsx
+++ index.tsx
@@ -15,5 +15,18 @@ const s = StyleSheet.create({
 
 export const sample1 = <Text style={s.work} />
 export const sample2 = <Text style={s.didNotWork} />
+// ^ this line generate error:
+// index.tsx:17:30 - error TS2322: Type 'RegisteredStyle<{ fontSize: number; fontWeight: string; marginTop: number; }>' is not assignable to type 'StyleProp<TextStyle>'.
+//   Type 'RegisteredStyle<{ fontSize: number; fontWeight: string; marginTop: number; }>' is not assignable to type 'RecursiveArray<false | TextStyle | RegisteredStyle<TextStyle> | null | undefined>'.
+//     Property 'length' is missing in type 'Number & { __registeredStyleBrand: { fontSize: number; fontWeight: string; marginTop: number; }; }'.
+
+// 17 export const sample2 = <Text style={s.didNotWork} />
+//                                 ~~~~~
+
+//   node_modules/@types/react-native/index.d.ts:907:5
+//     907     style?: StyleProp<TextStyle>;
+//             ~~~~~
+//     The expected type comes from property 'style' which is declared here on type 'IntrinsicAttributes & IntrinsicClassAttributes<Text> & Readonly<{ children?: ReactNode; }> & Readonly<TextProps>'
+
 export const sample3 = <Text style={{fontSize: 16, fontWeight: '900', marginTop: 5}} />
 
