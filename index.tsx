a37b4bcb5d8e38851dca959aee31909b177920e4
Vladimir Timofeev <vovkasm@gmail.com>
2018-09-28 11:17:52 +0300

    add comments

--- index.tsx
+++ index.tsx
@@ -4,8 +4,8 @@ import { StyleSheet, Text } from 'react-native'
 const s = StyleSheet.create({
   didNotWork: {
     fontSize: 16,
-    fontWeight: '900',
-    marginTop: 5,
+    fontWeight: '900', // if we comment this line, errors gone
+    marginTop: 5, // if this line commented, errors also gone
   },
   work: {
     fontSize: 18,
