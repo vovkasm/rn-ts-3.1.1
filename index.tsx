565e69acbb44e664ed11d9c368430bc9f6c7fd4e
Vladimir Timofeev <vovkasm@gmail.com>
2018-10-19 08:59:01 +0300

    update to latest version of ts, reduced example do not depend on @types/react-native

--- index.tsx
+++ index.tsx
@@ -1,32 +1,14 @@
-import React from 'react'
-import { StyleSheet, Text } from 'react-native'
 
-const s = StyleSheet.create({
+interface TextStyle extends ViewStyle { fontWeight?: "900"; }
+interface ViewStyle { marginTop?: number; }
+declare function create<T extends { [P: string]: TextStyle | ViewStyle }>(styles: T): T
+
+const s = create({
   didNotWork: {
-    fontSize: 16,
     fontWeight: '900', // if we comment this line, errors gone
     marginTop: 5, // if this line commented, errors also gone
   },
-  work: {
-    fontSize: 18,
-//    fontWeight: '900', // when uncommented also work
-  },
 })
-
-export const sample1 = <Text style={s.work} />
-export const sample2 = <Text style={s.didNotWork} />
-// ^ this line generate error:
-// index.tsx:17:30 - error TS2322: Type 'RegisteredStyle<{ fontSize: number; fontWeight: string; marginTop: number; }>' is not assignable to type 'StyleProp<TextStyle>'.
-//   Type 'RegisteredStyle<{ fontSize: number; fontWeight: string; marginTop: number; }>' is not assignable to type 'RecursiveArray<false | TextStyle | RegisteredStyle<TextStyle> | null | undefined>'.
-//     Property 'length' is missing in type 'Number & { __registeredStyleBrand: { fontSize: number; fontWeight: string; marginTop: number; }; }'.
-
-// 17 export const sample2 = <Text style={s.didNotWork} />
-//                                 ~~~~~
-
-//   node_modules/@types/react-native/index.d.ts:907:5
-//     907     style?: StyleProp<TextStyle>;
-//             ~~~~~
-//     The expected type comes from property 'style' which is declared here on type 'IntrinsicAttributes & IntrinsicClassAttributes<Text> & Readonly<{ children?: ReactNode; }> & Readonly<TextProps>'
-
-export const sample3 = <Text style={{fontSize: 16, fontWeight: '900', marginTop: 5}} />
-
+const f1: TextStyle = s.didNotWork
+const f2: ViewStyle = s.notExists
+// ^--- Property 'notExists' does not exist on type '{ didNotWork: { fontWeight: "900"; marginTop: number; }; }'.
